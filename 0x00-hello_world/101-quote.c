d22b2c6cfae853e8ea6d45e1ec00b8d7b85c81c9
Leon Munene <leonmunene254@gmail.com>
2023-03-16 08:34:09 -0700

    4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,5 @@
 int main(void)
 {
 const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-ssize_t len = sizeof(message) - 1;
-ssize_t ret = write(STDERR_FILENO, message, len);
-return ret != len;
+return (1);
 }
