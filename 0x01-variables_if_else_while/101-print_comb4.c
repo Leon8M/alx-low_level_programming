0b1928df3d6720c9a7df6a66974b9b5563dc136f
Leon Munene <leonmunene254@gmail.com>
2023-03-17 09:52:23 -0700

    6

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@
 int main() {
 int i, j, k;
   
-for (int i = 0; i < 8; i++)
+for (i = 0; i < 8; i++)
 {
-for (int j = i + 1; j < 9; j++)
+for (j = i + 1; j < 9; j++)
 {
-for (int k = j + 1; k < 10; k++)
+for (k = j + 1; k < 10; k++)
 {
 putchar(i + '0');
 putchar(j + '0');
