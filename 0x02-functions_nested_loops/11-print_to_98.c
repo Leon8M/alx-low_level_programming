bb5ff2c9b08cb0abc4789ad194b3863f0c5c224c
Leon Munene <leonmunene254@gmail.com>
2023-03-21 10:40:12 -0700

    3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,6 +3,12 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: the starting number
+ * print_number - prints an integer using _putchar
+ * @n: the integer to print
+ *
+ * Description: This function prints the digits of an integer using recursion
+ * and the _putchar function. If the integer is negative, it first prints a
+ * negative sign. If the integer is zero, it prints a single '0' character.
  */
 void print_number(int n);
 
