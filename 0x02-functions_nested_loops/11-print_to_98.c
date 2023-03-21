bae5ff9ea4180c63187faa5ff2b54fab7f1860de
Leon Munene <leonmunene254@gmail.com>
2023-03-21 10:36:03 -0700

    2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -33,14 +33,16 @@ print_number(98);
 _putchar('\n');
 }
 
-void print_number(int n) {
+void print_number(int n)
+{
 if (n < 0)
 {
 _putchar('-');
 n = -n;
 }
 
-if (n / 10) {
+if (n / 10)
+{
 print_number(n / 10);
 }
 
