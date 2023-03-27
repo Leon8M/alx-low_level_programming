2201ae61eced80b3b808d7e5d74a5585cb7a824d
Leon Munene <leonmunene254@gmail.com>
2023-03-27 07:54:39 -0700

    1

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,6 +7,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+/**                                                                                                                                                
+ *char *dest_start - write function
+ * @dest
+ * @src
+ */
 char *dest_start = dest;
 while (*src != '\0')
 {
