f486ddac1399c8f26c6ffab905404ec4aadd105a
Leon Munene <leonmunene254@gmail.com>
2023-03-27 09:39:58 -0700

    6

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,7 +20,7 @@ sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
 if (num > INT_MAX / 10 || (num == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-return sign == 1 ? INT_MAX : INT_MIN;
+return (sign == 1 ? INT_MAX : INT_MIN);
 num = num * 10 + (s[i] - '0');
 }
 else if (num != 0)
@@ -28,5 +28,5 @@ break;
 i++;
 }
 
-return num * sign;
+return (num * sign);
 }
