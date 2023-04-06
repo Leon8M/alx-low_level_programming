9aad659a957d6aa68bbc1266aa6a767fcf3933a9
Leon Munene <leonmunene254@gmail.com>
2023-04-06 12:27:43 -0700

    ii

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,22 +2,32 @@
 
 /**
  * is_prime_number - checks if a number is prime
+ *
  * @n: the number to check
  *
- * Return: 1 if n is prime, 0 otherwise
+ * Return: 1 if the number is prime, 0 otherwise
  */
 int is_prime_number(int n)
-{
-int i;
 
-if (n <= 1)
-return (0);
-
-for (i = 2; i < n; i++)
-{
-if (n % i == 0)
-return (0);
+if (n <= 1) 
+return 0;
+return is_prime_helper(n, 2);
 }
 
-return (1);
+/**
+ * is_prime_helper - helper function for is_prime_number
+ *
+ * @n: the number to check
+ * @i: the current divisor to check
+ *
+ * Return: 1 if the number is prime, 0 otherwise
+ */
+int is_prime_helper(int n, int i)
+{
+if (n % i == 0 && i != n)
+return 0;
+else if (i == n)
+return 1;
+else
+return is_prime_helper(n, i + 1);
 }
